9434c51cf0741b47b1eb00b10d9deb7c8a8c8695
TheWitherBane <Wither-Bane@users.noreply.github.com>
2020-03-24 12:53:54 +0000

    implemented code for Step2 of project article (at this stage, untested)

---
+++ src/gatherCaseData.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+import OpenBlender
+import pandas as pd
+import json
+%matplotlib inline
+action = 'API_getObservationsFromDataset'
+parameters = { 
+ 'token':'YOUR_TOKEN_HERE',
+ 'id_dataset':'5e6ac97595162921fda18076',
+ 'date_filter':{
+               "start_date":"2020-01-01T06:00:00.000Z",
+               "end_date":"2020-03-11T06:00:00.000Z"},
+ 'consumption_confirmation':'on' 
+}
+
+df_confirmed = pd.read_json(json.dumps(OpenBlender.call(action, parameters)['sample']), convert_dates=False, convert_axes=False).sort_values('timestamp', ascending=False)
+df_confirmed.reset_index(drop=True, inplace=True)
+df_confirmed.head(10)
