993d161e1f4cdacbdaa45f4005a0dfa37c28d85d
TheWitherBane <Wither-Bane@users.noreply.github.com>
2020-03-24 13:41:23 +0000

    Added comment-placeholder-thingy to mark the code written for step 2, and then ran the code (which didn't display graphs as i expected it to! hopefully it will do this by the end of me writing the base code for the project).

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
+#################################################
+#                   Step 2 Code below           #
+#################################################
+
 from matplotlib import pyplot as plt
 import OpenBlender
 import pandas as pd
 import json
-%matplotlib inline
+#%matplotlib inline
 action = 'API_getObservationsFromDataset'
+
+token0 = '5e7a020e9516296cb9efb249JGOrGBIApe2tyunHZp2BQf5VitHVys'
+
 parameters = { 
- 'token':'YOUR_TOKEN_HERE',
+ 'token':token0,
  'id_dataset':'5e6ac97595162921fda18076',
  'date_filter':{
                "start_date":"2020-01-01T06:00:00.000Z",
@@ -16,3 +23,5 @@ parameters = {
 df_confirmed = pd.read_json(json.dumps(OpenBlender.call(action, parameters)['sample']), convert_dates=False, convert_axes=False).sort_values('timestamp', ascending=False)
 df_confirmed.reset_index(drop=True, inplace=True)
 df_confirmed.head(10)
+
+
