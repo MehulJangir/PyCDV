5a97fae57e25759589962a07ec1358cbc955badd
TheWitherBane <Wither-Bane@users.noreply.github.com>
2020-03-24 13:44:30 +0000

    implemented 1st code snippet for step 3 of project article

--- src/main.py
+++ src/main.py
@@ -25,3 +25,36 @@ df_confirmed.reset_index(drop=True, inplace=True)
 df_confirmed.head(10)
 
 
+#################################################
+#                   Step 3 Code below           #
+#################################################
+
+
+action = 'API_getOpenTextData'
+parameters = {
+    'token':'YOUR_TOKEN_HERE',
+    'consumption_confirmation':'on',
+    'date_filter':{"start_date":"2020-01-01T06:00:00.000Z", 
+                   "end_date":"2020-03-10T06:00:00.000Z"},
+    'sources':[
+                # Wall Street Journal
+               {'id_dataset' : '5e2ef74e9516294390e810a9', 
+                 'features' : ['text']},
+                # ABC News Headlines
+               {'id_dataset':"5d8848e59516294231c59581", 
+                'features' : ["headline", "title"]},
+                # USA Today Twitter
+               {'id_dataset' : "5e32fd289516291e346c1726", 
+                'features' : ["text"]},
+                # CNN News
+               {'id_dataset' : "5d571b9e9516293a12ad4f5c", 
+                'features' : ["headline", "title"]}
+    ],
+    'aggregate_in_time_interval' : {
+              'time_interval_size' : 60 * 60 * 24
+    },
+    'text_filter_search':['covid', 'coronavirus', 'ncov']
+    
+}
+df_news = pd.read_json(json.dumps(OpenBlender.call(action, parameters)['sample']), convert_dates=False, convert_axes=False).sort_values('timestamp', ascending=False)
+df_news.reset_index(drop=True, inplace=True)
